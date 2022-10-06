538a7664e28e438d903b9262f242af9a6da8c473
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-10-06 01:15:13 -0700

    styling the time-blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,23 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 ///each time block for that day is color-coded to indicate whether it is in the past, present, or future.
 
+var currentTime = moment().format("HH");
+console.log(currentTime);
+
+//var calendarTime=document.getElementById("9");
+/*do(){
+  if (calendarTime===currentTime){
+    calendarTime.style.color. #ff6961;
+  } else if(calendarTime<=currentTime){
+    calendarTime.style.color. #d3d3d3;
+  } else
+  if(calendarTime>=currentTime){
+    calendarTime.style.color. #77dd77;
+  }
+},while(calendarTime<=9 && calendarTime>=17);
+
+*/
+
 //when clicked into a time block, an event can be entered.
 //event is written in a html text area in the time block.
 
