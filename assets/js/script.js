06eab56c9f9f10d2ad07813bc64938de04b98f5d
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-10-05 23:28:06 -0700

    add initial JavaScript file with pseudocode.

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+//pseudocode
+
+//Display the current day and date in the top of the calendar section.
+
+//use moment.js for date and time.
+
+//Add the time blocks for the day when the user scrolls down.
+//Use bootstrap flex layout for the time blocks
+
+///each time block for that day is color-coded to indicate whether it is in the past, present, or future.
+//Use Bootstrap color utilities to color code the time blocks
+
+//when clicked into a time block, an event can be entered.
+//event is written in a html text area in the time block.
+
+//When clicked on the save button for that time block ,the text for that event is saved in local storage.
+//use AddeventListener for the button click and localstorage.setitem for storing the event.
+
+//When the page is refreshed the page ,the saved events should persist.
+//localstorage.getitem
